db14de0ad786f9d0cadf5c32dad2c5d31633f79a
HeyEverton <everton.h.g.fonseca@gmail.com>
2024-02-24 20:32:40 -0300

    sweet alert added

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,25 +1,31 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createApp, h, DefineComponent } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
-import { mask } from 'vue-the-mask'
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {createApp, h, DefineComponent} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
+import {mask} from 'vue-the-mask'
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 import * as fas from '@fortawesome/free-solid-svg-icons';
+import InfiniteLoading from 'v3-infinite-loading'
+
+
+import VueSweetalert2 from 'vue-sweetalert2';
+
+// Styles
+import 'sweetalert2/dist/sweetalert2.min.css';
+import 'v3-infinite-loading/lib/style.css'
 
 Object.keys(fas).forEach((iconName: any) => {
     if (fas[iconName] !== 'fas') {
-        console.log(fas[iconName])
-
         library.add(fas[iconName]);
     }
 });
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import {createVuetify} from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
@@ -30,11 +36,13 @@ const vuetify = createVuetify({
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+    setup({el, App, props, plugin}) {
+        createApp({render: () => h(App, props)})
+            .use(VueSweetalert2)
             .use(plugin)
             .use(vuetify)
             .directive('mask', mask)
+            // components
             .component('font-awesome-icon', FontAwesomeIcon)
             .use(ZiggyVue)
             .mount(el);
