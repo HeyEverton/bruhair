2444da9b91bcbbafd39cf35751e9278fe9968d97
HeyEverton <everton.h.g.fonseca@gmail.com>
2024-03-14 22:10:30 -0300

    :sparkles: feat: filter orders by day, week and month + list all with the filter

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Carbon\Carbon;
 use App\Base\BaseService;
 use App\Models\Order;
 use Illuminate\Http\Response;
@@ -11,6 +12,17 @@ class OrderService extends BaseService
     public function __construct(private Order $order)
     {
         parent::__construct($order);
+    }
+
+    public function index(array $options = [])
+    {
+        return $this->order
+            ->with([
+                'employee',
+                'customer',
+                'product',
+            ])
+            ->paginate($options);
     }
 
     public function create(array $payload, string $shouldReturnResource = 'no_return'): int | object
@@ -24,5 +36,68 @@ class OrderService extends BaseService
             return $order;
         }
         return Response::HTTP_CREATED;
+    }
+
+    public function filter(string $date)
+    {
+        $query = $this->buildQuery($date);
+
+        return [
+            'data' => $query->with([
+                'employee',
+                'customer',
+                'product',
+            ])
+                ->orderBy('created_at', 'DESC')
+                ->get()
+        ];
+    }
+
+    private function buildQuery(string $date)
+    {
+        $query = Order::query();
+        switch ($date) {
+            case 'day':
+                $this->filterByDay($query);
+                break;
+            case 'week':
+                $this->filterByWeek($query);
+                break;
+            case 'month':
+                $this->filterByMonth($query);
+                break;
+            case 'all':
+                $this->allOrders($query);
+                break;
+        }
+
+        return $query;
+    }
+
+    private function filterByDay($query)
+    {
+        $today = Carbon::now()->format('Y-m-d');
+
+        $query->whereDate('created_at', $today);
+    }
+
+    private function filterByWeek($query)
+    {
+        $initial = now()->subDays(7)->format('Y-m-d');
+        $final = now()->format('Y-m-d');
+        $query->whereBetween('created_at', [
+            $initial . ' 00:00:00',
+            $final . ' 23:59:59'
+        ]);
+    }
+
+    private function filterByMonth($query)
+    {
+        $query->whereMonth('created_at', Carbon::now()->month);
+    }
+
+    private function allOrders($query)
+    {
+        $query->orderBy('created_at', 'DESC')->get();
     }
 }
--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,16 +1,17 @@
 import './bootstrap';
 import '../css/app.css';
 
-import {createApp, h, DefineComponent} from 'vue';
-import {createInertiaApp} from '@inertiajs/vue3';
-import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
-import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
-import {mask} from 'vue-the-mask'
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
-import * as fas from '@fortawesome/free-solid-svg-icons';
-import InfiniteLoading from 'v3-infinite-loading'
-import {vue3Debounce} from 'vue-debounce'
+import { createApp, h, DefineComponent } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { mask } from 'vue-the-mask'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+// import * as fas from '@fortawesome/free-solid-svg-icons';
+// import InfiniteLoading from 'v3-infinite-loading'
+import { vue3Debounce } from 'vue-debounce'
+import { faBrazilianRealSign, faArrowRotateLeft, faPaperPlane, faBan, faPlus, faPencil, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 
 import VueSweetalert2 from 'vue-sweetalert2';
@@ -19,14 +20,16 @@ import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import 'v3-infinite-loading/lib/style.css'
 
-Object.keys(fas).forEach((iconName: any) => {
-    if (fas[iconName] !== 'fas') {
-        library.add(fas[iconName]);
-    }
-});
+// Object.keys(fas).forEach((iconName: any) => {
+//     if (fas[iconName] !== 'fas') {
+//         library.add(fas[iconName]);
+//     }
+// });
+library.add(faBrazilianRealSign, faArrowRotateLeft, faPaperPlane, faBan, faPlus, faPencil, faTrash);
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 import 'vuetify/styles'
-import {createVuetify} from 'vuetify'
+import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
@@ -37,15 +40,15 @@ const vuetify = createVuetify({
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
-    setup({el, App, props, plugin}) {
-        createApp({render: () => h(App, props)})
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
             .use(VueSweetalert2)
             .use(plugin)
             .use(vuetify)
             .directive('mask', mask)
             // components
             .component('font-awesome-icon', FontAwesomeIcon)
-            .directive('debounce', vue3Debounce({lock: true}))
+            .directive('debounce', vue3Debounce({ lock: true }))
             .use(ZiggyVue)
             .mount(el);
     },
