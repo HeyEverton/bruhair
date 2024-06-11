f8c6c877454908161a0da0b916f9daf3d3babd76
HeyEverton <everton.h.g.fonseca@gmail.com>
2024-06-11 19:06:38 -0300

    :construction: wip: more products to pdv

--- resources/js/Pages/Pdv.vue
+++ resources/js/Pages/Pdv.vue
@@ -67,7 +67,6 @@ const create = () => {
 </script>
 
 <template>
-
     <Head title="Ponto de Venda" />
 
     <AuthenticatedLayout>
@@ -77,61 +76,97 @@ const create = () => {
             </h2>
         </template>
 
-
         <form @submit.prevent="create" class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg d-flex">
-                    <div class="flex flex-wrap w-full px-3 md:w-1/2 mb- md:mb-0">
+                <div
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg d-flex"
+                >
+                    <div
+                        class="flex flex-wrap w-full px-3 md:w-1/2 mb- md:mb-0"
+                    >
                         <InputLabel for="product_id" value="Cliente" />
 
-                        <select id="product_id"
+                        <select
+                            id="product_id"
                             class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
-                            v-model="form.customer_id">
+                            v-model="form.customer_id"
+                        >
                             <option value="">Selecione o Cliente</option>
-                            <option v-for="customer of customers" :value="customer.id">
+                            <option
+                                v-for="customer of customers"
+                                :value="customer.id"
+                            >
                                 {{ customer.name }}
                             </option>
                         </select>
-                        <InputError class="mt-2" :message="form.errors.customer_id" />
+                        <InputError
+                            class="mt-2"
+                            :message="form.errors.customer_id"
+                        />
                     </div>
 
                     <div class="w-full px-3 md:w-1/2">
                         <InputLabel for="employee_id" value="Funcionário" />
 
-                        <select v-model="form.employee_id" id="employee_id"
-                            class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500">
+                        <select
+                            v-model="form.employee_id"
+                            id="employee_id"
+                            class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
+                        >
                             <option value="">Selecione o Funcionário</option>
-                            <option v-for="employee of employees" :value="employee.id">
+                            <option
+                                v-for="employee of employees"
+                                :value="employee.id"
+                            >
                                 {{ employee?.name }}
                             </option>
                         </select>
-                        <InputError class="mt-2" :message="form.errors.employee_id" />
+                        <InputError
+                            class="mt-2"
+                            :message="form.errors.employee_id"
+                        />
                     </div>
 
                     <div class="w-full px-3 md:w-1/2">
                         <InputLabel for="product_id" value="Produto" />
 
-                        <select id="product_id"
+                        <select
+                            id="product_id"
                             class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
-                            v-model="form.product_id" @update:model-value="changeTotal">
+                            v-model="form.product_id"
+                            @update:model-value="changeTotal"
+                        >
                             <option value="">
                                 Selecione o Produto ou Serviço
                             </option>
-                            <option v-for="product of products" :value="product">
+                            <option
+                                v-for="product of products"
+                                :value="product"
+                            >
                                 {{ product?.name }} -
                                 {{ product?.price_formatted }}
                             </option>
                         </select>
-                        <InputError class="mt-2" :message="form.errors.product_id" />
+                        <InputError
+                            class="mt-2"
+                            :message="form.errors.product_id"
+                        />
                     </div>
                 </div>
 
-                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg d-flex">
+                <div
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg d-flex"
+                >
                     <div class="w-full px-3 md:w-1/2">
-                        <InputLabel for="payment_type" value="Forma de Pagamento" />
-                        <select id="payment_type"
+                        <InputLabel
+                            for="payment_type"
+                            value="Forma de Pagamento"
+                        />
+                        <select
+                            id="payment_type"
                             class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
-                            v-model="form.payment_type">
+                            v-model="form.payment_type"
+                        >
                             <option value="">
                                 Selecione a Forma de Pagamento
                             </option>
@@ -140,33 +175,57 @@ const create = () => {
                             <option value="3">PIX</option>
                             <option value="4">Dinheiro</option>
                         </select>
-                        <InputError class="mt-2" :message="form.errors.payment_type" />
+                        <InputError
+                            class="mt-2"
+                            :message="form.errors.payment_type"
+                        />
                     </div>
 
                     <div class="w-full px-3 md:w-1/2">
                         <InputLabel for="total" value="Total" />
-                        <TextInput id="total" type="text"
+                        <TextInput
+                            id="total"
+                            type="text"
                             class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
-                            v-model="form.total" required />
+                            v-model="form.total"
+                            required
+                        />
                         <InputError class="mt-2" :message="form.errors.total" />
                     </div>
                 </div>
 
-                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg d-flex">
+                <div
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg d-flex"
+                >
                     <div class="w-full px-3 md:w-full">
                         <InputLabel for="description" value="Descrição" />
-                        <textarea v-model="form.description" id="description" rows="4"
-                            class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" />
-                        <InputError class="mt-2" :message="form.errors.description" />
+                        <textarea
+                            v-model="form.description"
+                            id="description"
+                            rows="4"
+                            class="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
+                        />
+                        <InputError
+                            class="mt-2"
+                            :message="form.errors.description"
+                        />
                     </div>
                 </div>
             </div>
         </form>
 
-        <div class="w-full mt-5 overflow-hidden bg-white shadow-sm sm:rounded-lg d-flex">
-            <GeneralButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing"
-                buttonText="Nova Venda" variant="elevated" icon="fa-brazilian-real-sign" color="primary" @click="create"
-                :loading="loadingOrder" />
+        <div class="ml-15">
+            <GeneralButton
+                class="ms-4"
+                :class="{ 'opacity-25': form.processing }"
+                :disabled="form.processing"
+                buttonText="Nova Venda"
+                variant="elevated"
+                icon="fa-brazilian-real-sign"
+                color="primary"
+                @click="create"
+                :loading="loadingOrder"
+            />
         </div>
     </AuthenticatedLayout>
 </template>
