89e1893e61476e3059cd620c07393cd2562e6f4f
HeyEverton <everton.h.g.fonseca@gmail.com>
2024-08-04 12:49:19 -0300

    :ok_hand: removed commented code

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -37,10 +37,6 @@ class Order extends Model
         return $this->belongsTo(User::class, 'customer_id');
     }
 
-    // public function product()
-    // {
-    //     return $this->belongsTo(Product::class, 'product_id');
-    // }
 
     public function items(): HasMany
     {
