67def885453ad5e48a0d994fc8f5a826482e6eeb
HeyEverton <everton.h.g.fonseca@gmail.com>
2024-08-04 12:47:51 -0300

    :sparkles: feat: dinamic alert delayed payment

---
+++ app/Http/Middleware/ShareEnvVariables.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Symfony\Component\HttpFoundation\Response;
+
+class ShareEnvVariables
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        Inertia::share([
+            'delayed_payment' => env('DELAYED_PAYMENT', false),
+        ]);
+
+        return $next($request);
+    }
+}
